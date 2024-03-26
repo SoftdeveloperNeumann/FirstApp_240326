53c21ed92c47f6b09d739baa05806e267c76f426
Frank <f.neumann.berlin@googlemail.com>
2024-03-26 12:30:31 +0100

    Verwendung des OnClick Listeners

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.firstapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.view.View
+import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
+import com.example.firstapp.databinding.ActivityMainBinding
 
 // das war eine Zwischenlösung
 // import kotlinx.android.synthetic.main.activity_main.* diente der direkten Nutzung von ViewElementen aus der .xml
@@ -12,16 +12,46 @@ import androidx.appcompat.app.AppCompatActivity
 
 // Wir werden mit dem ViewBinding arbeiten. Zur Verwendung muss eine Anpassung in der Builddatei eingetragen werden
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val input = findViewById<EditText>(R.id.et_input)
-        val output = findViewById<TextView>(R.id.tv_output)
-        val btn = findViewById<Button>(R.id.btn_send)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+//        // Variante 1
+//        binding.btnSend.setOnClickListener { button : View ->
+//            binding.tvOutput.text = binding.etInput.text
+//            binding.etInput.text.clear()
+//        }
+
+        // Variante 2
+//        binding.btnSend.setOnClickListener(this)
+
+        // Variante 3
+        // Der Listener/ Funktionsaufruf wird in der Layoutdatei zugewiesen
+
+// Das ursprüngliche Vorgehen um mit Elementen der View zu arbeiten
+
+//        setContentView(R.layout.activity_main)
+//        val input = findViewById<EditText>(R.id.et_input)
+//        val output = findViewById<TextView>(R.id.tv_output)
+//        val btn = findViewById<Button>(R.id.btn_send)
+
+    }
+
+    override fun onClick(view: View?) {
+        binding.tvOutput.text = binding.etInput.text
+        binding.etInput.text.clear()
+    }
+
+    fun derClick(view: View) {
+        binding.tvOutput.text = binding.etInput.text
+        binding.tvOutput.append(" Hallo")
+        binding.etInput.text.clear()
     }
 }
